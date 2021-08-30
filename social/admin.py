9f25faa9af113a93abbdfdcab0d6f3f59589b8df
legionscript <legionscript@legionscript.com>
2021-08-30 18:42:08 -0400

    updated for deployment

--- social/admin.py
+++ social/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Post, UserProfile, Comment, Notification, ThreadModel
 
-admin.site.register(Post)
-admin.site.register(UserProfile)
-admin.site.register(Comment)
-admin.site.register(Notification)
-admin.site.register(ThreadModel)
+# Register your models here.
--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
     path('accounts/', include('allauth.urls')),
-    path('social/', include('social.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
